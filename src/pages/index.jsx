c10841ac077453c71f934781e05495d90da4090d
AeeUX <AndreasEriksen99@hotmail.com>
2021-08-11 18:39:04 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import Loadable from "react-loadable";
 import "../styles/home.scss";
 
-
 const loader = () => <div></div>;
 //
 const HomeLazy = Loadable({
-  loader: () => import('../containers/Main'),
+  loader: () => import("../containers/Main"),
   loading: loader,
 });
 
