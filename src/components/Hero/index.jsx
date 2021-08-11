87dd144e2a2a4f5f324c5ac1ee4c88bcc2f442af
AeeUX <AndreasEriksen99@hotmail.com>
2021-08-11 02:47:28 +0200

    Update index.jsx

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import HeroHeader from './HeroHeader'
 import './style.scss'
 
 export default function Hero() {
     return (
-        <div>
-            
+        <div className="hero-container">
+            <HeroHeader />
+            <div className="hero-media">                
+                <HeroCollage />
+                <HeroPhoneBlock />
+            </div>
+            <HeroUsedBy />
+            <HeroFooter />
         </div>
     )
 }
