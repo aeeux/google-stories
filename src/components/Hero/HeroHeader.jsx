ce35ef4b3c2f9f832a881841898ed15169fee4f5
AeeUX <AndreasEriksen99@hotmail.com>
2021-08-11 02:47:26 +0200

    Create HeroHeader.jsx

---
+++ src/components/Hero/HeroHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HeroHeader() {
+    return (
+        <div className="hero-text-section">
+            <h1 id="hero-text">Stories meet their widest audience ever</h1>
+        </div>
+    )
+}
