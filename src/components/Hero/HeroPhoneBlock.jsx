f834083ac48681a742053b76d9c51a9b28edcc40
AeeUX <AndreasEriksen99@hotmail.com>
2021-08-13 21:34:49 +0200

    Update HeroPhoneBlock.jsx

--- src/components/Hero/HeroPhoneBlock.jsx
+++ src/components/Hero/HeroPhoneBlock.jsx
@@ -4,6 +4,8 @@ import { gsap } from "gsap";
 import { useRef } from "react";
 import { useEffect } from "react";
 
+import './style.scss';
+
 const videoUrl =
   "https://kstatic.googleusercontent.com/files/c44f15bb7e678d651e18fdee3058f2948aa733849e0dea3daf7429bf0f77ec23bd670dba63e71739d5b53489c98689bdbb80c47cf55f44649d9d1bfdf3e4f0a0";
 
